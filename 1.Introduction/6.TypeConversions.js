e43d4181b9f75047ae7e1d16a77376f9e2eb84b3
Ravi D. Pansuriya <49712335+Ravipatel1999@users.noreply.github.com>
2022-05-15 16:38:32 +0530

    Type Conversions/Comparison operators

--- 1.Introduction/6.TypeConversions.js
+++ 1.Introduction/6.TypeConversions.js
@@ -8,8 +8,18 @@
 // Explicit Conversion - manual type conversion
 
 /*
-> JavaScript Implicit Conversion
 
-In certain situations, JavaScript automatically converts one data type to another (to the right type). This is known as implicit conversion.
+> JavaScript Implicit Conversion
+    In certain situations, JavaScript automatically converts 
+    one data type to another (to the right type). This is known as implicit conversion.
+
+*/
+
+
+/*
+
+> JavaScript Explicit Conversion
+    You can also convert one data type to another as per your needs.
+    The type conversion that you do manually is known as explicit type conversion.
 
 */
