03859c07904a73e4404d091f164e19cabbdac872
Ravi D. Pansuriya <49712335+Ravipatel1999@users.noreply.github.com>
2022-05-15 16:44:44 +0530

    Comparison and Logical Operators

--- 2.ControlFlow/ComparisonOperators.js
+++ 2.ControlFlow/ComparisonOperators.js
@@ -1,4 +1,20 @@
-/*=====================================
-    JavaScript Comparison Operators
-=======================================*/
+/*==================================================
+    JavaScript Comparison and Logical Operators
+====================================================*/
 
+
+
+// JavaScript Comparison Operators
+
+// Comparison operators compare two values and give back a boolean value: either true or false.
+
+/**
+ * ==	Equal to: true if the operands are equal	5==5; //true
+ * !=	Not equal to: true if the operands are not equal	5!=5; //false
+ * ===	Strict equal to: true if the operands are equal and of the same type	5==='5'; //false
+ * !==	Strict not equal to: true if the operands are equal but of different type or not equal at all	5!=='5'; //true
+ * >	Greater than: true if the left operand is greater than the right operand	3>2; //true
+ * >=	Greater than or equal to: true if the left operand is greater than or equal to the right operand	3>=3; //true
+ * <	Less than: true if the left operand is less than the right operand	3<2; //false
+ * <=	Less than or equal to: true if the left operand is less than or equal to the right operand	2<=2; //true
+ */
