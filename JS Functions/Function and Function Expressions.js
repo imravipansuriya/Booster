df1089c0061b3e3d5ba0739beb863a0352b84481
Ravi D. Pansuriya <49712335+Ravipatel1999@users.noreply.github.com>
2022-05-17 08:31:33 +0530

    Function

---
+++ JS Functions/Function and Function Expressions.js
@@ -0,0 +1,26 @@
+/*=========================
+    JavaScript Function
+===========================*/
+
+// A function is a block of code that performs a specific task.
+
+// Declaring a Function
+
+// The syntax to declare a function is:
+
+function nameOfFunction () {
+    // function body   
+}
+
+// declaring a function named greet()
+function greet() {
+    console.log("Hello there");
+}
+
+// Calling a Function
+
+// function call
+greet();
+
+
+// https://www.programiz.com/javascript/function
